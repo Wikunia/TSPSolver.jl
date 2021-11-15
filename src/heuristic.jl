9e3130618bf11c7941f21f0e7d4ccbe29188ebd9
Ole Kroeger <ole@zellerfeld.com>
2021-11-15 21:22:39 +0100

    fix and disallow edges

--- src/heuristic.jl
+++ src/heuristic.jl
@@ -1,18 +1,69 @@
 """
-    greedy(points, cost)
+    greedy(points, cost, fixed_edges=Edge[], disallowed_edges=Edge[])
 
 Create a tour which starts at point 1 and goes to the nearest unvisited neighbor in each step.
-Return the tour as a list of points and the cost of the tour
+Return the tour as a list of points and the cost of the tour.
+If no tour is possible return `nothing, NaN`
 """
-function greedy(points, cost)
+function greedy(points, cost, fixed_edges=zeros(Int, size(cost,1)), disallowed_edges=Dict{Int,Set{Int}}())
     N = length(points)
-    visited = Set{Int}(1)
+    visited = Set{Int}()
+    # the destination shouldn't be used before by some other edge
+    c = 0
+    for i in 1:N
+        if fixed_edges[i] != 0
+            push!(visited, fixed_edges[i])
+            c += 1
+        end
+    end
+    if c != length(visited)
+        return nothing, NaN
+    end
+    
     tour = Int[1]
     last_city = 1
+    push!(visited, 1)
     while length(tour) < N
-        last_city = argmin(j->j in visited ? Inf : cost[j, last_city], 1:N)
+        # if already fixed
+        if fixed_edges[last_city] != 0
+            last_city = fixed_edges[last_city]
+        else
+            last_city = get_next_city(last_city, visited, cost, disallowed_edges)
+            # no edge found
+            if last_city == 0
+                return nothing, NaN
+            end
+        end
         push!(visited, last_city)
         push!(tour, last_city)
     end
+    # the last edge doesn't match
+    if fixed_edges[last_city] != 0 && tour[1] != fixed_edges[last_city]
+        return nothing, NaN
+    end
     return tour, get_tour_cost(tour, cost)
+end
+
+function get_next_city(city, visited, cost, all_disallowed_edges)
+    disallowed_edges = get(all_disallowed_edges, city, nothing)
+    return get_next_city(city, visited, cost, disallowed_edges)
+end
+
+function get_next_city(city, visited, cost, ::Nothing)
+    return argmin(j->j in visited ? Inf : cost[j, city], 1:size(cost, 1))
+end
+
+function get_next_city(city, visited, cost, disallowed_edges::Set{Int})
+    min_val = Inf
+    min_idx = 0
+    for j in 1:size(cost,1) 
+        j in disallowed_edges && continue
+        j in visited && continue
+        val = cost[city, j]
+        if val < min_val
+            min_idx = j
+            min_val = val
+        end
+    end
+    return min_idx
 end
--- test/unit/main.jl
+++ test/unit/main.jl
@@ -14,6 +14,70 @@ end
     N = length(points)
     cost = [TSPSolver.euclidean_distance(points[i], points[j]) for i = 1:N, j = 1:N]
 
-    tree, lb = TSPSolver.greedy(points, cost)
+    tour, lb = TSPSolver.greedy(points, cost)
+    @test length(tour) == N
     @test lb > 118293.52381566973
+
+    # fix the first edges as it was in greedy anyway
+    fixed_edges = zeros(Int, N)
+    fixed_edges[tour[1]] = tour[2]
+    new_tour, new_lb = TSPSolver.greedy(points, cost, fixed_edges)
+    @test new_lb ≈ lb    
+    @test new_tour == tour 
+    
+    # fix a couple of edges and check that they were fixed correctly
+    fixed_edges = zeros(Int, N)
+    fixed_edges[1] = 2
+    fixed_edges[2] = 3
+    fixed_edges[4] = 8
+    tour, lb = TSPSolver.greedy(points, cost, fixed_edges)
+    c = 0
+    for i in 1:N
+        if fixed_edges[tour[i]] != 0
+            c += 1
+            @test tour[i+1] == fixed_edges[tour[i]]
+        end
+    end
+    @test c == count(i->i!=0, fixed_edges)
+
+    # fix a couple of edges and check that they were fixed correctly 
+    # also disallow some edges 
+    fixed_edges = zeros(Int, N)
+    fixed_edges[1] = 2
+    fixed_edges[2] = 3
+    fixed_edges[4] = 8
+
+    disallow_edges = Dict{Int, Set{Int}}()
+    disallow_edges[7] = Set([i for i in 1:N if !(i in [2,3,8,9])])
+    
+    tour, lb = TSPSolver.greedy(points, cost, fixed_edges, disallow_edges)
+    c = 0
+    for i in 1:N
+        if fixed_edges[tour[i]] != 0
+            c += 1
+            @test tour[i+1] == fixed_edges[tour[i]]
+        end
+
+        if tour[i] == 7
+            if i + 1 <= N
+                @test tour[i+1] == 9 # only 9 is not 
+            else
+                @test tour[1] == 9
+            end
+        end
+    end
+    @test c == count(i->i!=0, fixed_edges)
+
+    # disallow too many edges
+    fixed_edges = zeros(Int, N)
+    fixed_edges[1] = 2
+    fixed_edges[2] = 3
+    fixed_edges[4] = 8
+
+    disallow_edges = Dict{Int, Set{Int}}()
+    disallow_edges[7] = Set([i for i in 1:N if !(i in [2,3,8])])
+    
+    tour, lb = TSPSolver.greedy(points, cost, fixed_edges, disallow_edges)
+    @test tour === nothing
+    @test isnan(lb)
 end
